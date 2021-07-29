6bb0c6bef7726f025c71324a2d353ab8fb6de7f7
student <student@ip-172-31-52-92.us-west-2.compute.internal>
2021-07-29 16:55:59 -0700

    hm ngsjj

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,30 +1,59 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		String[] array = {"Hello","Hi","Hola","Aloha"};
-		//2. print the third element in the array
+		// 1. make an array of 5 Strings
+		String[] array = { "Hello", "Hi", "Hola", "Aloha" };
+		// 2. print the third element in the array
 		System.out.println(array[2]);
-		//3. set the third element to a different value
+		// 3. set the third element to a different value
 		array[2] = "Different";
-		//4. print the third element again
+		// 4. print the third element again
 		System.out.println(array[2]);
 		System.out.println("-------Separation--------");
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
+		// 5. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int i = 0; i < 4; i++) {
 			System.out.println(array[i]);
 		}
 		System.out.println("-------Separation--------");
-		//6. make an array of 50 integers
+		// 6. make an array of 50 integers
+		Random ran = new Random();
+		int[] array1 = new int[50];
+		for (int i = 0; i < 50; i++) {
+			int num = ran.nextInt(100);
+			array1[i] = num;
+
+		}
+		int nu = array1[0];
+		for (int i = 0; i < 50; i++) {
+			if (array1[i] < nu) {
+				nu = array1[i];
+			}
+
+		}
+		for (int i = 0; i < array1.length; i++) {
+			System.out.println(array1[i]);
+		}
 		
-		//7. use a for loop to make every value of the integer array a random number
-		
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		System.out.println("The smallest integer is " + nu);
+		for (int i = 0; i < 50; i++) {
+			if (array1[i] > nu) {
+				nu = array1[i];
+			}
+
+		}
+	
+		System.out.println("The largest integer is " + nu);
+		// 7. use a for loop to make every value of the integer array a random number
+
+		// 8. without printing the entire array, print only the smallest number in the
+		// array
+
+		// 9 print the entire array to see if step 8 was correct
+
+		// 10. print the largest number in the array.
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,8 +1,38 @@
 package arrays;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
+	Robot[] robots = new Robot[5];
+for (int i = 0; i < 5; i++) {
+	robots[i] = new Robot();
+	
+}
+int r =100;
+for (int i = 0; i < 5; i++) {
+	
+	robots[i].setY(500);
+	robots[i].setX(r);
+			r+=150;
+}
+Random ran = new Random();
+boolean raceStillGoing = true;
+while (raceStillGoing == true) {
+for (int i = 0; i < 5; i++) {
+	int num = ran.nextInt(50);
+	robots[i].move(num);
+	
+	if(robots[i].getY() < 0) {
+		raceStillGoing = false;
+		break;
+	}
+	
+}
+}
 	//2. create an array of 5 robots.
 
 	//3. use a for loop to initialize the robots.
@@ -19,4 +49,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
