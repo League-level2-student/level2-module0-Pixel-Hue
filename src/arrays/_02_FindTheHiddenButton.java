5f8aedb4c07c9a89f75b5c1e544c9542603870d2
student <student@ip-172-31-52-52.us-west-2.compute.internal>
2021-08-29 16:27:09 -0700

    yvgi

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] arrayb;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton = 0;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,39 +33,56 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String posn = JOptionPane.showInputDialog("Please enter a positive number");
+		int num = Integer.parseInt(posn);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		arrayb = new JButton[num];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
-		
+		for (int i = 0; i < num; i++) {
+			arrayb[i] = new JButton();
+			arrayb[i].addActionListener(this);
+			panel.add(arrayb[i]);
+		}
 		//9 add the panel to the window
+		window.add(panel);
 		
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Try and find the hidden button");
 		//13. initialize the hiddenButton variable to a random number less than the int created in step 3
-		
+		Random ran = new Random();
+		hiddenButton = ran.nextInt(num);
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
-
+arrayb[hiddenButton].setText("ME");
 		//15. Use Thread.sleep(1000); to pause the program.
 		//    Surround it with a try/catch - use Eclipse helper for this
-		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		//16. Set the text of the JButton located at hiddenButton to be blank.
-		
+		arrayb[hiddenButton].setText("");
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton buttonClicked = (JButton)e.getSource();
+	public void actionPerformed(ActionEvent l) {
+		JButton buttonClicked = (JButton)l.getSource();
 		
 		//17. if the hiddenButton is clicked, tell the user that they win.
-		
+		if (buttonClicked == arrayb[hiddenButton]) {
+			JOptionPane.showMessageDialog(null, "You win!");
+		}
 		//18. else tell them to try again
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong! Try again.");
+		}
 	}
 }
--- src/extra/SwingReview.java
+++ src/extra/SwingReview.java
@@ -29,62 +29,67 @@ public class SwingReview implements ActionListener, KeyListener {
 	JLabel label;
 	JButton button1;
 	JButton button2;
-
+int clicked;
 	public void run() throws Exception {
 		// 1. Initialize your JFrame variable as an object of the JFrame class
-		
+		frame = new JFrame();
 		// 2. Set your JFrame object to be visible
 		// JFrames are hidden by default! Make sure you are telling it to set visible, not asking if it is visible!
-
+frame.setVisible(true);
 		// 3. Run your program. Do you see your window? It's probably very
 		// small.
 
 		// 4. Set the default close operation to JFrame.EXIT_ON_CLOSE
 		// This makes the program actually close when we hit the red X in the corner! Without it, our program keeps
 		// running in the background even after we hit the red X.
-
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 5. Initialize your JPanel variable as an object of the JPanel class
-
+panel = new JPanel();
 		// 6. Initialize your JLabel variable as an object of the JLabel class
-
+label = new JLabel();
 		// 7. Set the text of the JLabel to a lovely greeting.
-
+label.setText("Hello, how was your day");
 		// 8. Add the JPanel object to the JFrame
-
+frame.add(panel);
 		// 9. Add the JLabel object to the JPanel
-
+panel.add(label);
 		// 10. Pack your JFrame.
 
 		// 11. Run your program again. Do you see your message?
 
 		// 12. Use the loadImage method to set the icon of the JLabel object.
-
+Icon icon = loadImage("java.png");
+label.setIcon(icon);
+frame.pack();
 		// 13. Re-pack the JFrame object.
 
 		// 14. Run your program again. Do you see the image?
 
 		// 15. Initialize your first JButton variable as an object of the JButton class
-
+button1 = new JButton();
 		// 16. Add the JButton object to the JPanel
-
+panel.add(button1);
 		// 17. Pack your JFrame
-
+frame.pack();
 		// 18. Add an action listener to the button
-
+button1.addActionListener(this);
 		// 19. Set the text of your button to "Click Me"
-
+button1.setText("Click Me");
 		// 20. Pack your JFrame
-
+frame.pack();
 		// 21. Add a second JButton and action listener to the button
-
+button2 = new JButton();
+panel.add(button2);
 		// 22. Set the text of your button to "No, Click Me"
-
+button2.setText("No, Click Me");
+button2.addActionListener(this);
 		// 23. Uncomment the 2 lines below (Ignore what they do for now)
-		// button1.setFocusable(false);
-		// button2.setFocusable(false);
+		 button1.setFocusable(false);
+		 button2.setFocusable(false);
 
 		// 29. Add a key listener to the frame
-
+frame.addKeyListener(this);
+frame.pack();
 	}
 
 	public ImageIcon loadImage(String fileName) {
@@ -95,13 +100,23 @@ public class SwingReview implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// 24. Uncomment the 2 lines below
-		// JButton buttonClicked = (JButton)e.getSource();
-		// System.out.println(buttonClicked.getText());
+		JButton buttonClicked = (JButton)e.getSource();
+		 System.out.println(buttonClicked.getText());
 
 		// 25. Check which button was clicked
 		// 26. If the "Click Me" button was clicked, use JOptionPane pop-up to say "Good
 		// Choice"
-
+		 if (buttonClicked == button1 && clicked == 3) {
+       button1.setText( "Stop Clicking me");
+       frame.pack();
+		}
+		 else if (buttonClicked == button1) {
+	JOptionPane.showMessageDialog(null, "Good Choice");
+	clicked++;
+}
+else if (buttonClicked == button2) {
+	JOptionPane.showMessageDialog(null, "A Better Choice");
+}
 		// 27. If the "No, Click Me button" was clicked, say "A Better Choice"
 
 		// 28. If the "Click Me" button is clicked 3 times, change the text of the button
@@ -111,16 +126,17 @@ public class SwingReview implements ActionListener, KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+	
 		// 30. Save the key typed by the user to a variable
 		// HINT: Look at the methods that KeyEvent e has
 
 		// 31. Use JOptionPane pop-up to tell the user which key they typed
-
+JOptionPane.showMessageDialog(null, "You clicked ");
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-
+		
 	}
 
 	@Override
